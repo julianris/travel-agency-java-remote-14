22609b0b39f4bcb8f34e62cc9b65f5be6a8e0263
Julian <julian.risler@gmail.com>
2022-08-14 12:40:13 +0200

    [change] - using converters and dtos by trip controller

--- src/main/java/com/sda/travelagency/controller/TripController.java
+++ src/main/java/com/sda/travelagency/controller/TripController.java
@@ -1,5 +1,7 @@
 package com.sda.travelagency.controller;
 
+import com.sda.travelagency.converter.TripConverter;
+import com.sda.travelagency.dto.TripDto;
 import com.sda.travelagency.entity.Trip;
 import com.sda.travelagency.service.TripService;
 import lombok.extern.slf4j.Slf4j;
@@ -17,22 +19,29 @@ public class TripController {
 
     private final TripService tripService;
 
-    public TripController(TripService tripService) {
+    private final TripConverter tripConverter;
+
+    public TripController(TripService tripService, TripConverter tripConverter) {
         this.tripService = tripService;
+        this.tripConverter = tripConverter;
     }
 
     @GetMapping
     public List<Trip> getAllTrips(){
         log.info("getting all trips");
 
-        return tripService.findAllTrips();
+        var entities= tripService.findAllTrips();
+        entities.stream()
+                .map(trip -> tripConverter.fromEntityToDto(trip))
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public Trip getTripById (@PathVariable("id") Long id){
+    public TripDto getTripById (@PathVariable("id") Long id){
         log.info("getting the trip by id: [{}]", id);
 
-        return tripService.findTripById(id);
+        var entity = tripService.findTripById(id);
+        return tripConverter.fromEntityToDto(entity);
     }
 
 }
--- src/main/java/com/sda/travelagency/converter/TripConverter.java
+++ src/main/java/com/sda/travelagency/converter/TripConverter.java
@@ -2,7 +2,9 @@ package com.sda.travelagency.converter;
 
 import com.sda.travelagency.dto.TripDto;
 import com.sda.travelagency.entity.Trip;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TripConverter implements Converter<Trip, TripDto> {
     @Override
     public TripDto fromEntityToDto(Trip trip) {
