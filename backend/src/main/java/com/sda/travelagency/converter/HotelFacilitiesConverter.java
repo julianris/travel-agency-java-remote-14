9265a8ce85124ae091d7f6bb6491f6888b025846
Julian <julian.risler@gmail.com>
2022-09-03 10:00:51 +0200

    [change] - hotel facilities converter in Java 8++ style - ready

--- backend/src/main/java/com/sda/travelagency/converter/HotelFacilitiesConverter.java
+++ backend/src/main/java/com/sda/travelagency/converter/HotelFacilitiesConverter.java
@@ -19,20 +19,35 @@ public class HotelFacilitiesConverter implements Converter<HotelFacilities, Hote
     }
 
     // till Java 8 implementation
+//    @Override
+//    public HotelFacilitiesDto fromEntityToDto(HotelFacilities entity) {
+//        List<AttractionDto> attractionDtos = new ArrayList<>();
+//        for (Attraction attraction: entity.getAttractions()){
+//            AttractionDto attractionDto = attractionConverter.fromEntityToDto(attraction);
+//            attractionDtos.add(attractionDto);
+//        }
+//        return new HotelFacilitiesDto(attractionDtos, entity.getApartmentFacilities());
+//    }
+
+    // since Java 8 style - more preferred
+
     @Override
     public HotelFacilitiesDto fromEntityToDto(HotelFacilities entity) {
-        List<AttractionDto> attractionDtos = new ArrayList<>();
-        for (Attraction attraction: entity.getAttractions()){
-            AttractionDto attractionDto = attractionConverter.fromEntityToDto(attraction);
-            attractionDtos.add(attractionDto);
-        }
+        var attractionsDtos = entity.getAttractions()
+                .stream()
+                .map(attraction -> attractionConverter.fromEntityToDto(attraction))
+                .toList();
 
-        return new HotelFacilitiesDto(attractionDtos, entity.getApartmentFacilities());
-
+        return new HotelFacilitiesDto(attractionsDtos, entity.getApartmentFacilities());
     }
 
     @Override
     public HotelFacilities fromDtoToEntity(HotelFacilitiesDto dto) {
-        return null;
+
+        var entities = dto.attractions()
+                .stream()
+                .map(attractionDto -> attractionConverter.fromDtoToEntity(attractionDto))
+                .toList();
+        return new HotelFacilities(entities, dto.apartmentFacilities());
     }
 }
