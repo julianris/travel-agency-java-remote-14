2cc21e3be542af6667caf129c4ed9ff94ce54de8
Julian <julian.risler@gmail.com>
2022-09-04 12:38:59 +0200

    [fix] - trusting angular frontend application - no cors issue anymore?!?

--- angular-frontend/src/app/components/trip-list/trip-list.component.ts
+++ angular-frontend/src/app/components/trip-list/trip-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TripService} from "../../services/trip-service/trip-service.service";
 import {TripDto} from "../../models/trips";
+import {tap} from "rxjs";
 
 @Component({
   selector: 'app-trip-list',
@@ -16,7 +17,9 @@ export class TripListComponent implements OnInit {
   ngOnInit(): void {
     //place for querying the backend and obtaining results
     this.tripService.getAllTrip()
-      //.pipe() // TODO: delay data to present that they are available some time later
+      .pipe(
+        tap(value => console.log("from server: " + value)) // tap() is for logging - we can see what's inside the stream of data
+      ) // TODO: delay data to present that they are available some time later
       .subscribe(value => this.trips = value)
   }
 
