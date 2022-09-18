b8352c406f48d8bf9a13016f284455e808a9b211
Julian <julian.risler@gmail.com>
2022-09-18 10:14:38 +0200

    [change] - adding bean validations to project and using on TripController

--- backend/src/main/java/com/sda/travelagency/controller/TripController.java
+++ backend/src/main/java/com/sda/travelagency/controller/TripController.java
@@ -6,6 +6,7 @@ import com.sda.travelagency.service.TripService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 // TODO: fix CORS in better way
@@ -25,21 +26,33 @@ public class TripController {
     }
 
     @GetMapping
-    public List<TripDto> getAllTrips(){
+    public List<TripDto> getAllTrips() {
         log.info("getting all trips");
 
-        var entities= tripService.findAllTrips();
+        var entities = tripService.findAllTrips();
         return entities.stream()
                 .map(trip -> tripConverter.fromEntityToDto(trip))
                 .toList();
     }
 
     @GetMapping("/{id}")
-    public TripDto getTripById (@PathVariable("id") Long id){
+    public TripDto getTripById(@PathVariable("id") Long id) {
         log.info("getting the trip by id: [{}]", id);
 
         var entity = tripService.findTripById(id);
         return tripConverter.fromEntityToDto(entity);
     }
 
+    @PostMapping
+    public TripDto createNewTrip(@Valid @RequestBody TripDto newTrip) {
+        log.info("trying to create new trip: [{}]", newTrip);
+        // convert dto to entity
+        var toSaveEntity = tripConverter.fromDtoToEntity(newTrip);
+
+        // store into db
+        var saved = tripService.createNewTrip(toSaveEntity);
+
+        // convert back to dto
+        return tripConverter.fromEntityToDto(saved);
+    }
 }
--- backend/src/main/java/com/sda/travelagency/dto/DestinationDto.java
+++ backend/src/main/java/com/sda/travelagency/dto/DestinationDto.java
@@ -1,9 +1,19 @@
 package com.sda.travelagency.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public record DestinationDto(
 
         Long id,
+        @NotNull
+        @Size(min =4)
         String country,
+        @NotNull
+        @Size(min =2)
         String city,
-        String hotelName) {
+        @NotNull
+        @Size(min =3, max = 256)
+        String hotelName
+) {
 }
